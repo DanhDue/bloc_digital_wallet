0127a0b344d355d4ca96d3cca0c26048fe938021
DanhDue ExOICTIF <pieter021505@gmail.com>
2026-01-12 18:33:09 +0700

    add the material to use Key in auto route.

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2026, one of DanhDue ExOICTIF projects. All rights reserved.
 
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 
 import 'features/authentication/presentation/pages/forgot_password_page.dart';
 import 'features/authentication/presentation/pages/login_page.dart';
